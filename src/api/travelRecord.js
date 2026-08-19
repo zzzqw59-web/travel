f6c318a51613eaa2ca1eb29931c16ab4c6a3c339
mute <zzzqw59@gmail.com>
2026-08-19 16:02:22 +0900

    study: axios 서버 연동

---
+++ src/api/travelRecord.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+// 서버 주소
+export const API_SERVER_HOST = "http://localhost:8080";
+
+// API 기본 경로
+const prefix = `${API_SERVER_HOST}/api/travelRecord`;
+
+// Travel Record 1건 조회
+export const getOne = async (no) => {
+    // HTTP GET 요청을 보내고 응답을 기다림
+    const response = await axios.get(`${prefix}/${no}`)
+
+    // 응답 데이터 반환
+    return response.data;
+}
+
+// Travel Record 목록 조회
+export const getList = async () => {
+    const response = await axios.get(prefix);
+
+    return response.data;
+}
